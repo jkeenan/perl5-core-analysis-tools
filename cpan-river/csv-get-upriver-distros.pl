435cc2c0944b5e26aef6d062d848995d670fd9dd
James E Keenan <jkeenan@cpan.org>
2017-11-08 09:48:55 -0500

    Add documentation in POD format.

--- cpan-river/csv-get-upriver-distros.pl
+++ cpan-river/csv-get-upriver-distros.pl
@@ -7,6 +7,53 @@ use Text::CSV;
 use Getopt::Long;
 use Cwd;
 
+=head1 NAME
+
+csv-get-upriver-distros.pl
+
+=head1 USAGE
+
+=over 4
+
+=item * Typical
+
+    csv-get-upriver-distros.pl --input=20171107.cpan.river.csv --count=2000 --verbose 
+
+Output will be written in current directory to file named 20171107-top-2000.txt.
+
+=item * Specify output directory
+
+    csv-get-upriver-distros.pl --input=20171107.cpan.river.csv --count=2000 --verbose \
+        outputdir=/path/to/output
+
+=item * Specify output filename
+
+    csv-get-upriver-distros.pl --input=20171107.cpan.river.csv --count=2000 --verbose \
+        output=/path/to/output/my-output.txt
+
+=back
+
+=head1 PREREQUISITES
+
+=head2 From CPAN 
+
+    Data::Dump
+    Text::CSV
+
+=head2 Input file
+
+A CSV file, typically named something like F<20171107.cpan.river.csv>, with the following format:
+
+    count,distribution,core_upstream_status,maintainers,top_5_downstream
+    28296,perl,,,"Carp:28278 ExtUtils-MakeMaker:28278 File-Path:28278 File-Temp:28278 PathTools:28278"
+    28278,Carp,blead-upstream,"DAPM DOM FLORA JESSE LBROCARD NWCLARK P5P RJBS ZEFRAM","Exporter:28278 File-Path:28278 File-Temp:28278 PathTools:28278 Pod-Simple:28278"
+    28278,Data-Dumper,blead-upstream,"EDAVIS GSAR ILYAM P5P SMUELLER","ExtUtils-MakeMaker:28278 CPAN-Meta:15945 ExtUtils-Manifest:15087 Module-Build:14986 ExtUtils-Config:8672"
+    28278,Encode,cpan-upstream,DANKOGAI,"ExtUtils-MakeMaker:28278 podlators:28278 CPAN-Meta:15945 URI:8582 Encode-Locale:7566"
+
+This file would typically be the output of F<compute_downstream_dag.pl>.
+
+=cut
+
 my ($target_count, $inputfile, $outputdir, $outputfile, $verbose) = ('') x 4;
 GetOptions(
     "count=i"       => \$target_count,
